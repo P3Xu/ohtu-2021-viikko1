e756776199aa5268be1784b36402d65586ffb1a5
Pekka <75335748+P3Xu@users.noreply.github.com>
2021-11-15 16:57:49 +0200

    Assignment 4: fix'd varasto.py

--- src/varasto.py
+++ src/varasto.py
@@ -37,18 +37,5 @@ class Varasto:
 
         return maara
 
-    def test_workflow(self):
-        if 1+1==2:
-            if 2+2==4:
-                for i in 10:
-                    if 3+3==6:
-                        jeejee = 0
-                    else:
-                        jeejee = 1
-        else:
-            msg = "Miksi ****ssa lausemäärä piti säätää 20 asti :D"
-            story = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque finibus, sapien vitae tristique finibus, magna lectus posuere sem, vulputate fermentum neque arcu sed mauris. Nam scelerisque tempor mi ut euismod. Morbi eu nisi aliquet urna imperdiet consequat. Donec tempor orci non magna malesuada rhoncus. Mauris dignissim tempor hendrerit. Nam pretium laoreet mi. Ut lobortis nec ante ut euismod. Aenean lacus dui, placerat eget accumsan non, cursus id ligula. Donec pulvinar libero id turpis luctus, mattis interdum mi suscipit. "
-
-
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
